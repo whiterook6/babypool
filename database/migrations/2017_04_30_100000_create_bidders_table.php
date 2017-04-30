d863db016e016a26e0bf727e5605014e5348340b
whiterook6 <whiterook6@gmail.com>
2017-04-30 09:39:24 -0700

    Adding migrations

--- database/migrations/2017_04_30_100000_create_bidders_table.php
+++ database/migrations/2017_04_30_100000_create_bidders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTables extends Migration {
+class CreateBiddersTable extends Migration {
     /**
      * Run the migrations.
      *
@@ -19,19 +19,6 @@ class CreateTables extends Migration {
             $table->unique('email');
             $table->index('created_at');
         });
-
-        Schema::create('bids', function (Blueprint $table){
-            $table->increments('id');
-            $table->integer('bidder_id')->unsigned();
-            $table->date('date');
-            $table->integer('value')->unsigned();
-            $table->tinyint('confirmed')->default(0);
-            $table->timestamps();
-
-            $table->foreign('bidder_id')->references('id')->on('bidders');
-            $table->index(['date', 'value']);
-            $table->index('created_at');
-        });
     }
 
     /**
@@ -40,7 +27,6 @@ class CreateTables extends Migration {
      * @return void
      */
     public function down(){
-        Schema::dropIfExists('bids');
         Schema::dropIfExists('bidders');
     }
 }
---
+++ database/migrations/2017_04_30_200000_create_bids_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBidsTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(){
+        Schema::create('bids', function (Blueprint $table){
+            $table->increments('id');
+            $table->integer('bidder_id')->unsigned();
+            $table->date('date');
+            $table->integer('value')->unsigned();
+            $table->tinyint('confirmed')->default(0);
+            $table->timestamps();
+
+            $table->foreign('bidder_id')->references('id')->on('bidders');
+            $table->index(['date', 'value']);
+            $table->index('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(){
+        Schema::dropIfExists('bids');
+    }
+}

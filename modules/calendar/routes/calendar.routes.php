59abc2862268e34cfcfa968d336e6f7905f9626a
whiterook6 <whiterook6@gmail.com>
2017-05-03 22:05:25 -0700

    moved routes better?

--- modules/calendar/routes/calendar.routes.php
+++ modules/calendar/routes/calendar.routes.php
@@ -2,11 +2,11 @@
 
 use Babypool\CalendarController;
 
-Route::get('/', CalendarController::class . '@calendar');
+Route::get('/',         CalendarController::class . '@calendar');
+Route::any('/finalize', CalendarController::class . '@finalize_bid');
 
 Route::group(['prefix' => 'calendar'], function () {
-	Route::any('/finalize', CalendarController::class . '@finalize_bid');
-	Route::get( '/',        CalendarController::class . '@calendar');
-	Route::get( '/{date}',  CalendarController::class . '@date');
-	Route::post('/{date}',  CalendarController::class . '@place_bid');
+	Route::get( '/',         CalendarController::class . '@calendar');
+	Route::get( '/{date}',   CalendarController::class . '@date');
+	Route::post('/{date}',   CalendarController::class . '@place_bid');
 });

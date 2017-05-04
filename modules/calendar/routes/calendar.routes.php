8b965f175579cdae2365a01be097a59da838359a
whiterook6 <whiterook6@gmail.com>
2017-05-03 22:02:31 -0700

    fixing route order

--- modules/calendar/routes/calendar.routes.php
+++ modules/calendar/routes/calendar.routes.php
@@ -5,8 +5,8 @@ use Babypool\CalendarController;
 Route::get('/', CalendarController::class . '@calendar');
 
 Route::group(['prefix' => 'calendar'], function () {
+	Route::any('/finalize', CalendarController::class . '@finalize_bid');
 	Route::get( '/',        CalendarController::class . '@calendar');
 	Route::get( '/{date}',  CalendarController::class . '@date');
 	Route::post('/{date}',  CalendarController::class . '@place_bid');
-	Route::any('/finalize', CalendarController::class . '@finalize_bid');
 });

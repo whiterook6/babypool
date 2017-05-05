598b8d63b56715ace4cb2b889afcc03d34430ae4
whiterook6 <whiterook6@gmail.com>
2017-05-04 21:22:39 -0700

    styling

--- resources/views/day.blade.php
+++ resources/views/day.blade.php
@@ -7,14 +7,14 @@
 				<h1>
 @isset($previous_date)
 					<div class="left form">
-						<a class="button"
+						<a class="button inline"
 							href="{{$previous_date}}"><span class="fa fa-chevron-left"></span> Previous Day</a>
 					</div>
 @endisset
 					{{$date_string}}
 @isset($date_string)
 					<div class="right form">
-						<a class="button"
+						<a class="button inline"
 							href="{{$next_date}}">Next Day <span class="fa fa-chevron-right"></span></a>
 					</div>
 @endisset
@@ -87,7 +87,7 @@
 				</div>
 				<div class="row">
 					<div class="col-12 form">
-						<a class="button"
+						<a class="button inline"
 							href="/calendar">Back</a>
 					</div>
 				</div>					

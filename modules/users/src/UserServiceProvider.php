90c6c14d2b54c551ccb9af0648e5dcf8ccc8acd4
whiterook6 <whiterook6@gmail.com>
2017-05-11 21:26:06 -0700

    Adding user service provider

---
+++ modules/users/src/UserServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Babypool\Providers;
+
+use Babypool\LoginController;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class UserServiceProvider extends ServiceProvider {
+	/**
+	 * Bootstrap the application services.
+	 */
+	public function boot() {
+		$this->publishes([
+			__DIR__.'/../migrations/' => base_path('/database/migrations'),
+		], 'migrations');
+
+		// $this->publishes([
+		// 	__DIR__.'/../factories/' => base_path('/database/factories'),
+		// ], 'factories');
+
+		$this->publishes([
+			__DIR__.'/../routes/' => base_path('/routes'),
+		], 'routes');
+	}
+
+	/**
+	 * Register the application services.
+	 */
+	public function register() {
+		$this->app->singleton(LoginController::class, function ($app) {
+			return new LoginController;
+		});
+	}
+}

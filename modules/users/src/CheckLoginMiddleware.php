fd55e10717ab0febd1dede2203d996dee5673bf6
whiterook6 <whiterook6@gmail.com>
2017-05-12 21:01:43 -0700

    Removing logger line

--- modules/users/src/CheckLoginMiddleware.php
+++ modules/users/src/CheckLoginMiddleware.php
@@ -9,7 +9,6 @@ class CheckLoginMiddleware {
 
 	public function handle($request, Closure $next){
 		if (!Auth::check()){
-			\Log::info('Not logged in?');
 			$path = $request->path();
 			return redirect("/login?continue={$path}");
 		} else {

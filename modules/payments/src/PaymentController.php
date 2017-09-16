fe25d7e04d24d1a87093c13b6094de9ccfb01197
whiterook6 <whiterook6@gmail.com>
2017-09-16 13:15:32 -0700

    Adding stripe exception handling.

--- modules/payments/src/PaymentController.php
+++ modules/payments/src/PaymentController.php
@@ -4,9 +4,16 @@ namespace Babypool;
 
 use Auth;
 use Babypool\BabbyController;
+use Exception;
 use Illuminate\Http\Request;
-use Stripe\Stripe;
 use Stripe\Charge;
+use Stripe\Error\ApiConnection;
+use Stripe\Error\Authentication;
+use Stripe\Error\Base;
+use Stripe\Error\Card;
+use Stripe\Error\InvalidRequest;
+use Stripe\Error\RateLimit;
+use Stripe\Stripe;
 
 class PaymentController extends BabbyController {
 	public function pay(Request $request){
@@ -19,13 +26,46 @@ class PaymentController extends BabbyController {
 		$owing = intval(decrypt($request->input('owing_encrypted')));
 		$token = $request->input('token');
 
-		Stripe::setApiKey('sk_test_BQokikJOvBiI2HlWgH4olfQ2');
-		$charge = Charge::create([
-			'amount' => $owing * 100,
-			'currency' => 'cad',
-			'description' => 'Babypool Payment',
-			'source' => $token,
-		]);
+		try {
+			Stripe::setApiKey('sk_test_BQokikJOvBiI2HlWgH4olfQ2');
+		
+		} catch (Authentication $e){
+			throw new Exception('Authentication error. Your card has not been charged.');
+		}
+
+		try {
+			$charge = Charge::create([
+				'amount' => $owing * 100,
+				'currency' => 'cad',
+				'description' => 'Babypool Payment',
+				'source' => $token,
+			]);
+		
+		} catch(Card $e) {
+			$body = $e->getJsonBody();
+			$error  = $body['error'];
+
+			if (array_key_exists('message', $error)){
+				throw new Exception('Stripe reports the card is declined. The specific error message is: '.$error['message']);
+			} else {
+				throw new Excpetion('Stripe reports the card is declined. There was no specific error message.');
+			}
+		
+		} catch (RateLimit $e) {
+			throw new Exception('Stripe reports too many users are making API requests at once. Your card has not been charged.');
+		
+		} catch (InvalidRequest $e) {
+			throw new Exception('Stripe reports invalid parameters were sent from the baby pool. Your card has not been charged.');
+		
+		} catch (Authentication $e) {
+			throw new Exception('Stripe reports an authentication error (after setting its API key.) Your card has not been charged.');
+		
+		} catch (ApiConnection $e) {
+			throw new Exception('Stripe reports a network communication error. Your card has not been charged.');
+		
+		} catch (Base $e) {
+			throw new Exception('Stripe reports a generic error. Your card has not been charged.');
+		}
 
 		$payment = Payment::create_from_charge($user, $charge);
 		return redirect(action('\\'.UserController::class.'@me'));

bd525c4cf19941369d7c6e59dfb976d06792617e
whiterook6 <whiterook6@gmail.com>
2017-09-28 20:17:32 -0700

    adding unfurling

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -8,6 +8,10 @@
 		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"
 			rel="stylesheet"
 			type="text/css">
+		<meta property="og:url" content="https://babby.graboski.ca/"/>
+		<meta property="og:title" content="Online Baby Pool for John and Caitlin Oram's newborn."/>
+		<meta property="og:description" content="When is babby formed?"/>
+		<meta property="og:site_name" content="Babby Pool"/>
 	</head>
 	<body>
 		<div class="wrapper">

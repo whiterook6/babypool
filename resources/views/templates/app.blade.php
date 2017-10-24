a5c4160375e8f79d83b0db01724f106c808ac165
whiterook6 <whiterook6@gmail.com>
2017-10-23 21:37:45 -0700

    Adding favicon

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -12,6 +12,8 @@
 		<meta property="og:title" content="Online Baby Pool for John and Caitlin Oram's newborn."/>
 		<meta property="og:description" content="When is babby formed?"/>
 		<meta property="og:site_name" content="Babby Pool"/>
+		<meta property="og:image" content="/favicon-96x96.png" />
+		<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
 	</head>
 	<body>
 		<div class="wrapper">

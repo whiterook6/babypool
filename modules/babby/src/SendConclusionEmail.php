e6855bb56ddcc94073558f49205ce385f9047959
whiterook6 <whiterook6@gmail.com>
2017-11-10 20:42:52 -0800

    Sorting and finding closest two days, or exact date.

--- modules/babby/src/SendConclusionEmail.php
+++ modules/babby/src/SendConclusionEmail.php
@@ -2,6 +2,7 @@
 
 namespace Babypool;
 
+use Babypool\Bid;
 use DateTime;
 use Illuminate\Console\Command;
 
@@ -30,10 +31,10 @@ class SendConclusionEmail extends Command {
 	 */
 	public function handle(){
 		$birth_date = null;
+		$date_of_birth_string = "";
 
 		do {
 			$today_string = date('Y-m-d');
-			$date_of_birth_string = "";
 
 			do {
 				$date_of_birth_string = $this->ask('What is the date of birth? (YYYY-MM-DD)', $today_string);			
@@ -43,11 +44,44 @@ class SendConclusionEmail extends Command {
 			$check_input = $birth_date->format('Y-m-d');
 			$english = $birth_date->format('l jS \of F Y');
 		} while ($check_input === $date_of_birth_string && !($this->confirm("Confirm: {$english}?")));
+
+		$max_date_diff = 30;
+		$current_date_diff = 0;
+		$bids = collect($this->get_highest_bid($date_of_birth_string));
+
+		while ($bids->isEmpty() && $current_date_diff < $max_date_diff){
+			$current_date_diff ++ ;
+			$timestamp = strtotime($date_of_birth_string);
+
+			$l_date = date('Y-m-d', strtotime("-{$current_date_diff} days", $timestamp));			
+			$r_date = date('Y-m-d', strtotime("+{$current_date_diff} days", $timestamp));
+
+			$bids = $this->get_highest_bids(collect([$l_date, $r_date]));
+		}
+
+		// $bids has the winning bid or bids, if split between two dates.
+
 		// prompt for date of baby
 		// Calculate winner
 		// email winner
 
 		// calculate who owes money
 		// email those who owe money
+	}
+
+	private function get_highest_bid($date_string){
+		return Bid::with('user')->where('date', $date_string)->highest()->first();
+	}
+
+	private function get_highest_bids($date_strings){
+		$bids = collect();
+		$date_strings->each(function($date_string) use ($bids){
+			$bid = $this->get_highest_bid($date_string);
+			if (!is_null($bid)){
+				$bids->push($bid);
+			}
+		});
+
+		return $bids;
 	}
 }
